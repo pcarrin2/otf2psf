dbdc706905f2a421acf8406bb9ad62ba40f38581
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2024-11-11 13:56:53 -0600

    Add reporting function to ttf parser

--- src/report.rs
+++ src/report.rs
@@ -3,35 +3,35 @@ use std::fmt::Formatter;
 
 #[derive(Debug)]
 pub struct Report {
-    grapheme: String,
+    character: char,
     glyph_type: GlyphType,
     height: u32,
     width: u32,
 }
 
 impl Report {
-    fn new(grapheme: String, glyph_type: GlyphType, height: u32, width: u32) -> Self {
-        return Self{grapheme, glyph_type, height, width}
+    pub fn new(character: char, glyph_type: GlyphType, height: u32, width: u32) -> Self {
+        return Self{character, glyph_type, height, width}
     }
 }
 
 impl Display for Report {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        write!(f, "{}: {}, {} x {} px", self.grapheme, self.glyph_type, self.height, self.width)
+        write!(f, "{}: {}, {} x {} px", self.character, self.glyph_type, self.height, self.width)
     }
 }
 
 #[derive(Debug)]
 pub enum GlyphType {
-    FromEmbeddedBitmap { format: ab_glyph::GlyphImageFormat },
-    Rasterized,
+    EmbeddedBitmap { format: ab_glyph::GlyphImageFormat },
+    Vector,
 }
 
 impl Display for GlyphType {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match self {
-            Self::FromEmbeddedBitmap{format} => write!(f, "TTF embedded bitmap (format: {:?})", format),
-            Self::Rasterized => write!(f, "rasterized from vector font"),
+            Self::EmbeddedBitmap{format} => write!(f, "TTF embedded bitmap (format: {:?})", format),
+            Self::Vector => write!(f, "Vector glyph"),
         }
     }
 }
--- src/ttf_parser.rs
+++ src/ttf_parser.rs
@@ -8,6 +8,8 @@ use std::path::Path;
 use crate::glyph;
 use crate::errors::TtfParserError;
 use crate::errors::GlyphError;
+use crate::report::Report;
+use crate::report::GlyphType;
 
 /// A parser that creates `Glyph`s from a TTF/OTF font and a character set.
 #[derive(Debug)]
@@ -45,6 +47,24 @@ impl TtfParser {
             None => self.rasterize(character),
         }
     }
+
+    fn report_char(&self, character: char) -> Report {
+        let glyph_id = self.font.glyph_id(character);
+        let glyph_image = self.font.font.glyph_raster_image2(glyph_id, self.font.height().ceil() as u16);
+        let (glyph_type, height, width) = match glyph_image {
+            None => {
+                let glyph: ab_glyph::Glyph = self.font
+                    .glyph_id(character)
+                    .with_scale_and_position(self.font.height(), point(0.0, 0.0));
+                let width = self.font.h_advance(glyph.id).ceil() as u32;
+                let height = self.font.height() as u32;
+                (GlyphType::Vector, height, width)
+            }
+            Some(g) => (GlyphType::EmbeddedBitmap{format: g.format}, g.height.into(), g.width.into()),
+        };
+        return Report::new(character, glyph_type, height, width);
+    }
+
     
     fn find_embedded_bitmap(&self, character: char) -> Option<glyph::Glyph> {
         let glyph_id = self.font.glyph_id(character);
